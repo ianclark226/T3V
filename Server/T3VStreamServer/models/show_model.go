467ae3197b14456b86b4c69bcc6de72bcaba4669
Ian Clark <ianclark226@gmail.com>
2025-11-30 12:56:49 -0500

    Added more fields to show model

--- Server/T3VStreamServer/models/show_model.go
+++ Server/T3VStreamServer/models/show_model.go
@@ -10,19 +10,11 @@ type Ranking struct {
 	RankingName  string `bson:"ranking_name" json:"ranking_name" validate:"required"`
 }
 
-// type Episode struct {
-// 	ID            bson.ObjectID `bson:"_id,omitempty" json:"_id,omitempty"`
-// 	ShowID        bson.ObjectID `bson:"show_id,omitempty" json:"show_id,omitempty"`
-// 	EpisodeNumber int           `bson:"episode_number" json:"episode_number"`
-// 	Title         string        `bson:"title" json:"title"`
-// 	Description   string        `bson:"description" json:"description"`
-// 	AirDate       string        `bson:"air_date" json:"air_date"`
-// 	Duration      int           `bson:"duration_minutes" json:"duration_minutes"`
-// }
-
 type Show struct {
 	ShowID      int    `json:"show_id" bson:"show_id"`
 	Title       string `json:"title" bson:"title"`
 	PosterPath  string `json:"poster_path" bson:"poster_path"`
+	Channel       []Channel       `bson:"channel" json:"channel" validate:"required,dive"`
 	AdminReview string `json:"admin_review" bson:"admin_review"`
+	Ranking     Ranking       `bson:"ranking" json:"ranking" validate:"required"`
 }
