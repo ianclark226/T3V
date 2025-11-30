6215be640daff5bde6d1637ca524f35ca388a72b
Ian Clark <ianclark226@gmail.com>
2025-11-30 14:04:53 -0500

    edit shows component

--- Client/t3v-client/src/components/shows/Shows.jsx
+++ Client/t3v-client/src/components/shows/Shows.jsx
@@ -1,17 +1,23 @@
-import Show from "../show/Show"
+import Show from "./Show";
 
-const Shows = ({shows, updateShowReview, message}) => {
+const Shows = ({ shows, updateShowReview, message }) => {
   return (
     <div className="container mt-4">
-        <div className="row">
-            {shows && shows.length > 0 ? shows.map((show) => (
-                <Show key={show._id} updateShowReview={updateShowReview} show={show}/>
-            ))
-            : <h2>{message}</h2>
-        }
-        </div>
+      <div className="row">
+        {shows && shows.length > 0 ? (
+          shows.map((show) => (
+            <Show
+              key={show.show_id}  // FIXED!
+              updateShowReview={updateShowReview}
+              show={show}
+            />
+          ))
+        ) : (
+          <h2>{message}</h2>
+        )}
+      </div>
     </div>
-  )
-}
+  );
+};
 
-export default Shows
+export default Shows;
